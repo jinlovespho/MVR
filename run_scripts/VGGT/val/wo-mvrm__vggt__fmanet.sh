5219a8c881f778c6d7388018a7874ca901989d0f
jinlovespho <msjchr922@gmail.com>
2026-05-26 17:12:34 +0900

    260526 s20 evaluaing vggt in da3 pipeline

---
+++ run_scripts/VGGT/val/wo-mvrm__vggt__fmanet.sh
@@ -0,0 +1,3 @@
+
+
+time CUDA_VISIBLE_DEVICES=4 python -m depth_anything_3.bench.evaluator --config run_configs/VGGT/val/wo-mvrm__vggt__fmanet.yaml
---
+++ run_scripts/VGGT/val/wo-mvrm__vggt__vae-mvd.sh
@@ -0,0 +1,3 @@
+
+
+time CUDA_VISIBLE_DEVICES=6 python -m depth_anything_3.bench.evaluator --config run_configs/VGGT/val/wo-mvrm__vggt__vae-mvd.yaml
